3bd765a31e68b05c94a7852217fc8c02f6b94fdd
Okubo <aspastrana990@gmail.com>
2020-05-25 20:08:42 -0300

    Set up Project

---
+++ src/Templates/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className="nav">
+            <div className="socialMedia">
+
+            </div>
+            <div className="logDiv">
+
+            </div>
+        </div>
+    )
+}
+
+export default NavBar
